29291b2f980bfdec693040fd0a22157c17f21d07
tony12str <al222310293.com>
2025-06-21 22:45:52 -0600

    🆙 Subiendo nuevas modificaciones de Data-X-POS-2 desde la carpeta de estadías

--- Data-X-POS-2/backend/server.js
+++ Data-X-POS-2/backend/server.js
@@ -1,7 +1,12 @@
-require('dotenv').config();
+require('dotenv').config({ path: __dirname + '/.env' });
+
 const express = require('express');
 const connectDB = require('./config/db');
 const cors = require('cors');
+const path = require("path");
+const session = require('express-session');
+const cartRoutes = require('./routes/cartRoutes');
+
 
 connectDB();
 
@@ -17,15 +22,23 @@ const userRoutes = require('./routes/userRoutes');
 const productRoutes = require('./routes/productRoutes');
 const saleRoutes = require('./routes/saleRoutes');
 const viewsRoutes = require("./routes/viewsRoutes");
+app.use(session({
+  secret: process.env.SESSION_SECRET || 'equipocore123',
+  resave: false,
+  saveUninitialized: false,
+  cookie: { maxAge: 1000 * 60 * 60 } // 1 hora
+}));
 
 app.use("/", viewsRoutes);
 app.use('/api/users', userRoutes);
 app.use('/api/products', productRoutes);
 app.use('/api/sales', saleRoutes);
+app.use('/cart', cartRoutes);
+
 
 // 🎨 Motor de vistas y estáticos
 app.set("view engine", "ejs");
-app.set("views", "./backend/views");
+app.set("views", path.join(__dirname, "views"));
 app.use(express.static("frontend"));
 
 app.listen(5000, () => console.log('🔥 Servidor corriendo en el puerto 5000'));
